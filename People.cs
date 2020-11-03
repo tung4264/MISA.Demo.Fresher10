06cb9cabf08f8597f8e27c0549a900bea293d004
tung4264 <60508821+tung4264@users.noreply.github.com>
2020-11-03 14:56:48 +0700

    Update People.cs

--- People.cs
+++ People.cs
@@ -6,5 +6,15 @@ namespace MISA.DemoTFS
 {
     class People
     {
+        private string PeopleCode;
+        private string PeopleName;
+        private string Emal;
+        private DateTime DateOfBirth;
+
+        public string PeopleCode1 { get => PeopleCode; set => PeopleCode = value; }
+        public string PeopleName1 { get => PeopleName; set => PeopleName = value; }
+        public string Emal1 { get => Emal; set => Emal = value; }
+        public DateTime DateOfBirth1 { get => DateOfBirth; set => DateOfBirth = value; }
+    }
     }
 }
